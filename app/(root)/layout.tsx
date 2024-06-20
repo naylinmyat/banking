7e70d070f1f5f89f8ef48d204dd3c2399d952c23
naylinmyat <naylinmyat04@gmail.com>
2024-06-20 17:51:22 +0700

    Implement auth, appwrite, and sentry

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,15 +1,17 @@
 import MobileNav from "@/components/MobileNav";
 import Sidebar from "@/components/SideBar"
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 import Image from 'next/image';
+import { redirect, useRouter } from "next/navigation";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = await getLoggedInUser();
 
-  const loggedIn = { firstName: 'Nay Lin', lastName: 'Myat' };
-
+  if(!loggedIn) redirect('/sign-in')
   return (
     <main className="flex h-screen w-full font-inter">
         <Sidebar user={loggedIn} />
